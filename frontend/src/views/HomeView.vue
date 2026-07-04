64519d2e734928b29b794d4ada37d3a6211ed890
xqyveteyio <xqyveteyio@outlook.com>
2026-07-05 03:09:37 +0800

    update

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,14 +1,34 @@
 <template>
     <div class="home-view">
-        <WallpaperCarousel/>
+        <WallpaperCarousel :thumbs="wallpapers" @selectWallpaper="onSelectWallpaper" :current_wallpaper="current_wallpaper"/>
     </div>
 </template>
 
 <script setup lang="ts">
 import WallpaperCarousel from '@/components/WallpaperCarousel.vue';
-import { onMounted } from 'vue';
+import { ref, onMounted } from 'vue';
 import { setupTray } from '@/utils/tray';
 import { quitApp } from '@/utils/lifecycle';
+
+const current_wallpaper = ref<string>()
+const wallpapers = [
+    "https://images4.alphacoders.com/135/thumb-1920-1357925.png",
+    "https://images8.alphacoders.com/136/thumb-1920-1362046.png",
+    "https://images7.alphacoders.com/101/thumb-1920-1012056.jpg",
+    "https://images3.alphacoders.com/138/thumb-1920-1386673.jpg",
+    "https://images2.alphacoders.com/140/thumb-1920-1407959.png",
+    "https://images5.alphacoders.com/132/thumb-1920-1324025.jpeg",
+    "https://images.alphacoders.com/129/thumb-1920-1293319.png",
+    "https://images7.alphacoders.com/127/thumb-1920-1277360.jpg",
+    "https://images4.alphacoders.com/140/thumb-1920-1409039.png",
+    "https://images5.alphacoders.com/139/thumb-1920-1395932.jpg",
+    "https://images4.alphacoders.com/140/thumb-1920-1401145.png"
+]
+
+const onSelectWallpaper = (url: string) => {
+    current_wallpaper.value = url
+}
+
 
 onMounted(() => {
     setupTray()
